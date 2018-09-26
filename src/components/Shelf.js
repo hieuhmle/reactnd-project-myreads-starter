abc79cec0e7af323b5f6d6511a5007630612a956
Hieu Le <hieuhmle@gmail.com>
2018-09-26 00:09:13 -0500

    Update <Shelf/> component

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,8 +1,24 @@
 import React from 'react'; 
 
 const Shelf = (props) => {
+    const { title, books } = props;
+
+    const renderBooks = () => {
+        return books.map(book => (
+            <li key={book.id}></li>
+            // TODO: Add BOOK component
+        ))
+    }
+
     return (
-        <div></div>
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{title}</h2>
+            <div className="bookshelf-books">
+            <ol className="books-grid">
+                
+            </ol>
+            </div>
+        </div>
     );
 }
 
