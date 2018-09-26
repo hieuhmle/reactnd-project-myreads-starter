f724650c0382d4fd6f51b599b4ad5d98e7f57dd6
Hieu Le <hieuhmle@gmail.com>
2018-09-25 23:57:09 -0500

    Decomposition
    Create <BookCase/>, <Shelf/>, <Book/> and <BookSearch/> components

---
+++ src/components/BookCase.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+const BookCase = (props) => {
+    return (
+        <div></div>
+    );
+}
+
+export default BookCase;
---
+++ src/components/BookSearch.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+const BookSearch = (props) => {
+    return (
+        <div></div>
+    );
+}
+
+export default BookSearch;
