23d96b78023556eebe64e3641036132bf5d13a15
Hieu Le <hieuhmle@gmail.com>
2018-09-26 00:04:05 -0500

    Update <Book/> component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,8 +1,25 @@
 import React from 'react'; 
 
 const Book = (props) => {
+    const { imageLinks, title, authors, shelf } = props;
+
     return (
-        <div></div>
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${imageLinks.thumbnail})` }}></div>
+                <div className="book-shelf-changer">
+                    <select defaultValue={shelf}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                </div>
+            </div>
+            <div className="book-title">{title}</div>
+            <div className="book-authors">{authors.reduce((str, author) => str + author + '\n')}</div>
+        </div>
     );
 }
 
