ef3a77959902f592bb7903deefa54355db890017
Hieu Le <hieuhmle@gmail.com>
2018-09-26 00:18:11 -0500

    Update <BookCase/> component

--- src/components/BookCase.js
+++ src/components/BookCase.js
@@ -1,8 +1,39 @@
 import React from 'react'; 
 
 const BookCase = (props) => {
+    const { books } = props;
+
+    const shelves = [
+        {id: 'currentlyReading', title: 'Currently Reading'},
+        {id: 'wantToRead', title: 'Want To Read'},
+        {id: 'read', title: 'read'}
+    ];
+
+    const filterBooksByShelf = (shelf) => {
+        return books.filter(book => book.shelf === shelf);
+    }
+
+    const renderShelves = () => {
+        return shelves.map(shelf => (
+            <li key={shelf.id}></li>
+            // TODO: Add Shelf Component
+        ));
+    }
+
     return (
-        <div></div>
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <ol>
+
+                </ol>
+            </div>
+            <div className="open-search">
+              
+            </div>
+        </div>
     );
 }
 
